f957f7a82de5fa0f7dd4052c7a3d4394bc7d67b0
Lewis Chambers <lewcha@ceh.ac.uk>
2024-07-23 14:15:55 +0100

    Finished script for renaming package

--- rename-package.sh
+++ rename-package.sh
@@ -1,4 +1,10 @@
 package_rgx='[_a-zA-Z0-9\-]+'
-oldname=$([_a-zA-Z0-9\-]+)
+oldname=$(grep -E "^ *name *= *\"${package_rgx}\"" pyproject.toml | grep -Eo "\"${package_rgx}\"" | sed s/\"//g)
+
+if [ -z "$oldname" ]; then
+    echo "old name not found"
+    exit 1
+fi
+
 find tests src docs .github/workflows pyproject.toml README.md -type f -exec sed -i s/$oldname/${1}/g {} +
-mv src/myproject $1
+mv src/$oldname src/$1

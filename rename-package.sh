62fcfdee6f991d001c5d9c3d015fffb922369780
Lewis Chambers <lewcha@ceh.ac.uk>
2024-07-23 14:08:01 +0100

    made script executable

--- rename-package.sh
+++ rename-package.sh
@@ -1,4 +1,4 @@
 package_rgx='[_a-zA-Z0-9\-]+'
 oldname=$([_a-zA-Z0-9\-]+)
-find tests src docs pyproject.toml README.md -type f -exec sed -i s/$oldname/${1}/g {} +
+find tests src docs .github/workflows pyproject.toml README.md -type f -exec sed -i s/$oldname/${1}/g {} +
 mv src/myproject $1

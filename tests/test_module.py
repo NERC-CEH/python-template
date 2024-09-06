b8fd64e30bd9c0c7453fdef00694ed5e6c6f8d76
Josh Holland <joshol@ceh.ac.uk>
2024-09-06 12:49:09 +0100

    Simplify tests and run ruff check/format over the whole repo
    Remove the parameterized dependency, as pytest has the required
    features built-in.

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,27 +1,15 @@
-import unittest
+import pytest
+
 from mypackage.module import add_int
-from parameterized import parameterized
-
-class TestModuleMethods(unittest.TestCase):
-
-    def test_errors(self):
-
-        with self.assertRaises(TypeError, msg="Expected integer argument for y."):
-            add_int(1, "3")
-        
-        with self.assertRaises(TypeError, msg="Expected integer argument for x."):
-            add_int({1,2}, 5)
-
-    
-    @parameterized.expand([
-        [1, 2, 3],
-        [-4, 10, 6],
-        [1000, 100, 1100]
-    ])
-    def test_result(self, x, y, expected):
-
-        self.assertEqual(add_int(x, y), expected)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_errors() -> None:
+    with pytest.raises(TypeError):
+        add_int(1, "3")
+    with pytest.raises(TypeError):
+        add_int({1, 2}, 5)
+
+
+@pytest.mark.parametrize("x,y,expected", [[1, 2, 3], [-4, 10, 6], [1000, 100, 1100]])
+def test_result(x: int, y: int, expected: int) -> None:
+    assert add_int(x, y) == expected
